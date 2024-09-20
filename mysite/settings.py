39eb53a517822f593d806707922c55a91f94fd41
VictoriaB <bodnarchukvv180588@gmail.com>
2024-09-20 14:34:32 +0200

    Update Python version in runtime.txt

--- mysite/settings.py
+++ mysite/settings.py
@@ -127,3 +127,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+import dj_database_url
+DATABASES = {
+    'default': dj_database_url.config(default='sqlite:///db.sqlite3')
+}
