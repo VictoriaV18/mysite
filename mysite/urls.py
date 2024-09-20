b04fceaeca910876ecae1db1429b7f5e3123623a
VictoriaB <bodnarchukvv180588@gmail.com>
2024-09-20 15:30:25 +0200

    Fix URL routing for polls app

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,9 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path("polls/", include("polls.urls")),
+path('', RedirectView.as_view(url='/polls/', permanent=True)),
     path("admin/", admin.site.urls),
 ]
 if settings.DEBUG:
